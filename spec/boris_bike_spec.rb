92d0f191942ab7a27a4552aa3dcb687cff54ad8e
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-10-19 16:33:44 +0100

    create 'can release a bike' test and make it pass

--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -1,5 +1,8 @@
 require 'boris_bike'
 
 describe DockingStation do
-  
+  it 'can release a bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
