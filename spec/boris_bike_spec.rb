3a722e91e38f1d0067a9114bfdaee5682f1bbc15
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-10-19 15:45:38 +0100

    set up spec file to describe docking station

--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -1,5 +1,3 @@
-describe 'DockingStation' do # just a description
-  it 'releases a bike' do # it block describing something
-    docking_station = DockingStation.new
-  end
+describe DockingStation do # just a description
+  
 end
